2f6c18f41ded211150da7f8b81b3508a38c181a5
Aderinola Opemipo Emmanuel <aderinolaopemipo@gmail.com>
2023-08-29 17:38:19 +0100

    added the mobile navigation menu

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-document.querySelector("img.hamburger").addEventListener("click", function() {
-    document.querySelector(".nav-links").classList.toggle("hide");
+let hamburgerIcon = $(".hamburger-icon");
+let closeIcon = $(".close-icon");
+let navMenu = $(".navigation-menu");
+let links = $(".nav-links a");
+
+hamburgerIcon.click(()=> {
+    setTimeout(()=> {
+        hamburgerIcon.addClass("hide");
+        closeIcon.removeClass("hide");
+        navMenu.removeClass("hide");
+    }, 300);
+});
+
+closeIcon.click(()=> {
+    setTimeout(()=> {
+        hamburgerIcon.removeClass("hide");
+        closeIcon.addClass("hide");
+        navMenu.addClass("hide");
+    }, 300);
+});
+
+links.click(()=> {
+    hamburgerIcon.removeClass("hide");
+    closeIcon.addClass("hide");
+    navMenu.addClass("hide");
 });
